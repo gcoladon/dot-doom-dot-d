2f5514669c84d3b2972d27826b4fcbe7cfc8dc11
Greg Coladonato <greg@pilot.ai>
2021-05-27 16:09:25 -0700

    removed commented out redundant vulpea code

--- config.el
+++ config.el
@@ -931,69 +931,6 @@ Return the commands created, as a list of symbols."
 
 ;; https://gist.github.com/d12frosted/a60e8ccb9aceba031af243dff0d19b2e
 
-
-;; (defun vulpea-project-p ()
-;;   "Return non-nil if current buffer has any todo entry.
-;; TODO entries marked as done are ignored, meaning the this
-;; function returns nil if current buffer contains only completed
-;; tasks."
-;;   (seq-find                                 ; (3)
-;;    (lambda (type)
-;;      (eq type 'todo))
-;;    (org-element-map                         ; (2)
-;;        (org-element-parse-buffer 'headline) ; (1)
-;;        'headline
-;;      (lambda (h)
-;;        (org-element-property :todo-type h)))))
-
-;; (defun vulpea-project-update-tag ()
-;;     "Update PROJECT tag in the current buffer."
-;;     (when (and (not (active-minibuffer-window))
-;;                (vulpea-buffer-p))
-;;       (save-excursion
-;;         (goto-char (point-min))
-;;         (let* ((tags (vulpea-buffer-tags-get))
-;;                (original-tags tags))
-;;           (if (vulpea-project-p)
-;;               (setq tags (cons "project" tags))
-;;             (setq tags (remove "project" tags)))
-;;           (unless (eq original-tags tags)
-;;             (apply #'vulpea-buffer-tags-set (seq-uniq tags)))))))
-
-;; (defun vulpea-buffer-p ()
-;;   "Return non-nil if the currently visited buffer is a note."
-;;   (and buffer-file-name
-;;        (string-prefix-p
-;;         (expand-file-name (file-name-as-directory org-roam-directory))
-;;         (file-name-directory buffer-file-name))))
-
-;; (defun vulpea-project-files ()
-;;     "Return a list of note files containing 'project' tag." ;
-;;     (seq-uniq
-;;      (seq-map
-;;       #'car
-;;       (org-roam-db-query
-;;        [:select [nodes:file]
-;;         :from tags
-;;         :left-join nodes
-;;         :on (= tags:node-id nodes:id)
-;;         :where (like tag (quote "%\"project\"%"))]))))
-
-;; (defun vulpea-agenda-files-update (&rest _)
-;;   "Update the value of `org-agenda-files'."
-;;   (setq org-agenda-files (vulpea-project-files)))
-
-;; (add-hook 'find-file-hook #'vulpea-project-update-tag)
-;; (add-hook 'before-save-hook #'vulpea-project-update-tag)
-;; (advice-add 'org-agenda :before #'vulpea-agenda-files-update)
-
-;; (dolist (file (org-roam--list-all-files))
-;;   (message "processing %s" file)
-;;   (with-current-buffer (or (find-buffer-visiting file)
-;;                            (find-file-noselect file))
-;;     (vulpea-project-update-tag)
-;;     (save-buffer)))
-
 (defun vulpea-project-p ()
   "Return non-nil if current buffer has any todo entry.
 TODO entries marked as done are ignored, meaning the this
