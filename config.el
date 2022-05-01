fd4904d11de942d54499cf0aee48de10ed5a1127
Greg Coladonato <gcoladonato3@gatech.edu>
2022-05-01 15:11:30 -0700

    commenting org-roam-ui back in

--- config.el
+++ config.el
@@ -534,20 +534,20 @@
 (modify-syntax-entry ?“ "(”")
 (modify-syntax-entry ?” ")“")
 
-;; (use-package! websocket
-;;     :after org-roam)
+(use-package! websocket
+    :after org-roam)
 
-;; (use-package! org-roam-ui
-;;     :after org-roam ;; or :after org
-;; ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-;; ;;         a hookable mode anymore, you're advised to pick something yourself
-;; ;;         if you don't care about startup time, use
-;; ;;  :hook (after-init . org-roam-ui-mode)
-;;     :config
-;;     (setq org-roam-ui-sync-theme t
-;;           org-roam-ui-follow t
-;;           org-roam-ui-update-on-save t
-;;           org-roam-ui-open-on-start t))
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
 
 (defun gpc/copy-todos-from-email ()
   (interactive)
