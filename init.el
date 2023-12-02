54db0aa5c74b270c0ce9003e0cdeef70e222e23f
Greg Coladonato <gcoladonato3@gatech.edu>
2023-12-01 16:36:01 -0800

    adding gnuplot

--- init.el
+++ init.el
@@ -154,7 +154,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2 +noter)
+       (org +roam2 +noter +gnuplot)
        ;; (org +roam +noter)         ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
