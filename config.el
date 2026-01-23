ca371ec1bc86d8bc60ac104766ce07730413e853
Greg Coladonato <gcoladon@gmail.com>
2026-01-22 18:57:04 -0800

    stop processing Perplexity citations

--- config.el
+++ config.el
@@ -991,11 +991,16 @@ It puts a todo to read this article near the top of the hackernews node."
   "Convert MARKDOWN-TEXT to Org format using Pandoc."
   (with-temp-buffer
     (insert markdown-text)
+
+    ;; Starting in Jan 2026, Preplexity no longer sent citations with the copy
+    ;; of the answer. So this is obsolete now.
+    ;;
     ;; I think I prefer putting each of these on their own line for readability
     ;; and debugging purposes.
-    (gpc/insert-citations-before-citations)
-    (gpc/scor "sed '/^Citations:/,$s/^\\[\\(.*\\)\\]/\\1. /'")
-    (gpc/scor "sed 's/^Citations:$/## Citations:\\n/'")
+    ;; (gpc/insert-citations-before-citations)
+    ;; (gpc/scor "sed '/^Citations:/,$s/^\\[\\(.*\\)\\]/\\1. /'")
+    ;; (gpc/scor "sed 's/^Citations:$/## Citations:\\n/'")
+
     (gpc/scor "/usr/local/bin/pandoc -f markdown -t org --wrap=none")
     (gpc/scor "sed '/^:PROPERTIES:/,/^:END:/d'")
     (gpc/scor "sed 's/\\\\\\\\$//'")
